7e01c9defd8ad86718857fc684a949dce43e5d70
natedogg484 <nathangschmidt@gmail.com>
2018-12-22 13:40:28 -0500

    getting close

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+var friends = [
+    {
+        name: "Steve",
+        photo: "http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            4,
+            1
+        ]
+
+    },
+    {
+        name: "Jon",
+        photo: "http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg",
+        scores: [
+            5, 4, 3, 2, 1, 1, 2, 3, 4, 5
+        ]
+
+    },
+    {
+        name: "Mark",
+        photo: "http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg",
+        scores: [
+            1, 2, 3, 1, 1, 1, 3, 4, 5, 4
+        ]
+
+    },
+    {
+        name: "Marie",
+        photo: "http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg",
+        scores: [
+            4, 1, 2, 3, 3, 2, 2, 1, 3, 4
+        ]
+
+    },
+    {
+        name: "Amy",
+        photo: "http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg",
+        scores: [
+            4, 1, 2, 3, 5, 4, 3, 1, 2, 1
+        ]
+
+    },
+    {
+        name: "Joe",
+        photo: "http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg",
+        scores: [
+            5, 4, 3, 1, 2, 4, 3, 2, 1, 4
+        ]
+
+    },
+    {
+        name: "Melissa",
+        photo: "http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg",
+        scores: [
+            4, 5, 5, 5, 5, 2, 1, 3, 4, 2
+        ]
+
+    }
+]
+
+
+module.exports = friends;
